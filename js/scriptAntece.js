2059f3c62d9b2af81cb2138599edb39e7ae019cb
DotExe-mx <56658504+DotExe-mx@users.noreply.github.com>
2021-05-19 13:49:38 -0500

    Add files via upload

---
+++ js/scriptAntece.js
@@ -0,0 +1,9 @@
+$(".capaMostrar").click(function() {
+    $('.capaMostrar').animate({ opacity: 0 }, 1000);
+});
+$(".capaMostrar2").click(function() {
+    $('.capaMostrar2').animate({ opacity: 0 }, 1000);
+});
+$(".capaMostrar3").click(function() {
+    $('.capaMostrar3').animate({ opacity: 0 }, 1000);
+});
